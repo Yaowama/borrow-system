45e29a6395b242ae518eb94346fa1772bf80c9a1
yaowaman <yaowaman.chul@mail.kmutt.ac.th>
2026-04-21 15:07:08 +0700

    fix SSL for session store

--- config/session.js
+++ config/session.js
@@ -4,9 +4,11 @@ require("dotenv").config();
 
 const sessionStore = new MySQLStore({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true }
 });
 
 module.exports = session({
