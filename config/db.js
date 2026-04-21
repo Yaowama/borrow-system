1b0768a568db4740cc91c923fd40ae9cd6de812f
yaowaman <yaowaman.chul@mail.kmutt.ac.th>
2026-04-21 15:08:42 +0700

    fix SSL for db and session

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-
-  timezone: "+07:00",   // 🔥 เพิ่มบรรทัดนี้
-
+  timezone: "+07:00",
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  ssl: { rejectUnauthorized: true }  
 });
 
 module.exports = db;
