ce7de63a7ef858c9a6ecffcaf777acc1b9ba46f0
yaowaman <yaowaman.chul@mail.kmutt.ac.th>
2026-04-23 11:16:51 +0700

    fix sql_mode on connection

--- config/db.js
+++ config/db.js
@@ -3,19 +3,16 @@ require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 4000,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   timezone: "+07:00",
   waitForConnections: true,
   connectionLimit: 10,
-  multipleStatements: true
-});
-
-// ปิด only_full_group_by ทันทีหลังเชื่อม
-db.on('connection', (connection) => {
-  connection.query("SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'");
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = db;
