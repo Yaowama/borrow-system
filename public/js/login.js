b86eccc2b6666d842a7ab45bc8c904d1dcc0a5d8
yaowaman <yaowaman.chul@mail.kmutt.ac.th>
2026-04-28 17:30:33 +0700

    edit device

--- public/js/login.js
+++ public/js/login.js
@@ -40,3 +40,17 @@ window.addEventListener("pageshow", () => {
   const loader = document.getElementById("globalLoader");
   if (loader) loader.classList.add("hidden");
 });
+
+document.querySelectorAll(".toggle-pw").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const input = btn.previousElementSibling;
+    const icon = btn.querySelector("i");
+    if (input.type === "password") {
+      input.type = "text";
+      icon.className = "fa-solid fa-eye-slash";
+    } else {
+      input.type = "password";
+      icon.className = "fa-solid fa-eye";
+    }
+  });
+});
